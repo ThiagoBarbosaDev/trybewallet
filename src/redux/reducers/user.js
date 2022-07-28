09727d04f5cee037d5faca496922e6db42877126
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-07-28 15:08:23 -0300

    feat: redux store, redux reducers (todo: action cases), redux provider

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, { email, type }) => {
+  switch (type) {
+  // case value:
+  //   return email;
+  default:
+    return state;
+  }
+};
+
+// user: {
+//   email: '', // string que armazena o email da pessoa usuária
+// },
+
+export default user;
